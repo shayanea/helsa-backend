2521269831570c78ebed2629b2d3e8d78eefa109
Shayan Araghi <shayanaraghi@live.com>
2020-11-07 23:41:48 +0330

    add zarinpal

--- api/payment/controllers/payment.js
+++ api/payment/controllers/payment.js
@@ -1,8 +1,102 @@
-'use strict';
+const ZarinpalCheckout = require("zarinpal-checkout");
+const zarinpal = ZarinpalCheckout.create(
+  "1fdb4262-8955-11e9-aeb9-000c29344814"
+);
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const findPackagePriceById = (id) => {
+  switch (Number(id)) {
+    case 1:
+      return { title: "۱ ساله", price: 54000 };
+      break;
+    case 2:
+      return { title: "۱ ماهه", price: 7000 };
+      break;
+    case 3:
+      return { title: "۳ ماهه", price: 16500 };
+      break;
+    case 4:
+      return { title: "۶ ماهه", price: 30000 };
+      break;
+    default:
+      return { title: "", price: 0 };
+      break;
+  }
+};
+
+module.exports = {
+  pay: async (ctx, next) => {
+    const result = await strapi.services.payment.create({
+      createDate: new Date(),
+      packageId: ctx.request.body.packageId,
+      userId: ctx.request.body.userId,
+    });
+    const zarinResult = await zarinpal
+      .PaymentRequest({
+        Amount: findPackagePriceById(ctx.request.body.packageId).price,
+        CallbackURL: `http://helsawell.com/zarinpal?token=${ctx.request.header.authorization}&id=${result.id}`,
+        Description: `خرید اشتراک ${
+          findPackagePriceById(ctx.request.body.packageId).title
+        }`,
+        Email: "shayanaraghi@live.com",
+        Mobile: "09127113556",
+      })
+      .then((response) => {
+        return response;
+      })
+      .catch((err) => {
+        return err;
+      });
+    return ctx.send(zarinResult);
+  },
+  validate: async (ctx, next) => {
+    const result = await strapi.services.payment.find({
+      _id: ctx.request.body.id,
+    });
+    const zarinResult = await zarinpal
+      .PaymentVerification({
+        Authority: ctx.request.body.authority,
+        Amount: findPackagePriceById(result.packageId).price,
+      })
+      .then((response) => {
+        if (response.status !== 100) {
+          console.log("Empty!");
+        } else {
+          console.log(`Verified! Ref ID: ${response.RefID}`);
+        }
+        strapi.services.payment.edit(
+          { _id: ctx.request.body.id },
+          {
+            authority: ctx.request.body.authority,
+            refId: response.RefID,
+            status: response.status,
+          }
+        );
+        // strapi.plugins["email"].services.email
+        //   .send({
+        //     to: "admin@strapi.io",
+        //     from: "برنامه هلساول <no-reply@helsawell.com>",
+        //     replyTo: "no-reply@strapi.io",
+        //     subject: "Use strapi email provider successfully",
+        //     text: "Hello world! 1",
+        //     html: "Hello world! 2",
+        //   })
+        //   .then((res) => {
+        //     console.log("success", res.data);
+        //   })
+        //   .catch((err) => {
+        //     console.log("error", err[0].messages[0]);
+        //   });
+        return { ...response, packageId: result.packageId };
+      })
+      .catch((err) => {
+        return err;
+      });
+
+    return ctx.send(zarinResult);
+  },
+};
--- api/payment/services/payment.js
+++ api/payment/services/payment.js
@@ -1,8 +1,58 @@
-'use strict';
-
+"use strict";
+const ZarinpalCheckout = require("zarinpal-checkout");
+const zarinpal = ZarinpalCheckout.create(
+  "1fdb4262-8955-11e9-aeb9-000c29344814"
+);
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+const findPackagePriceById = (id) => {
+  switch (Number(id)) {
+    case 1:
+      return { title: "۱ ساله", price: 54000 };
+      break;
+    case 2:
+      return { title: "۱ ماهه", price: 7000 };
+      break;
+    case 3:
+      return { title: "۳ ماهه", price: 16500 };
+      break;
+    case 4:
+      return { title: "۶ ماهه", price: 30000 };
+      break;
+    default:
+      return { title: "", price: 0 };
+      break;
+  }
+};
+
+module.exports = {
+  pay: async (ctx, next) => {
+    // const package = findPackagePriceById(ctx.request.body.packageId);
+    const result = await strapi.services.payment.add({
+      createDate: new Date(),
+      packageId: ctx.request.body.packageId,
+      userId: ctx.request.body.userId,
+    });
+    const zarinResult = await zarinpal
+      .PaymentRequest({
+        Amount: findPackagePriceById(ctx.request.body.packageId).price,
+        CallbackURL: `http://helsawell.com/zarinpal?token=${ctx.request.header.authorization}&id=${result.id}`,
+        Description: `خرید اشتراک ${
+          findPackagePriceById(ctx.request.body.packageId).title
+        }`,
+        Email: "shayanaraghi@live.com",
+        Mobile: "09127113556",
+      })
+      .then((response) => {
+        return response;
+      })
+      .catch((err) => {
+        return err;
+      });
+
+    return zarinResult;
+  },
+};
